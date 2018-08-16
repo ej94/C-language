fa201c7094bb741867ada4a4954478d432b3fb23
ej94 <amefawg66@gmail.com>
2018-08-16 11:19:10 +0800

    test

--- how to use strlen funtion.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-
-int main(void)
-{
-	char mystring[]="GO GO GO";
-	printf("The length of this string is %d",strlen(mystring));
-	return 0;
-}
--- show how Null character works.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-	printf("The character \0 is used to terminated a string.");
-	return 0;
-}
